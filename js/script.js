5526c539d510c6502a15a303cdcfda155ea83bf7
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-03 12:35:15 +0100

    adds main files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+Vue.config.devtools = true; 
+
+window.addEventListener("DOMContentLoaded", function(){
+    const vueApp = new Vue ({
+        el: "#root",
+        data: {
+            message: "ciao mondo"
+        }
+    })
+})
