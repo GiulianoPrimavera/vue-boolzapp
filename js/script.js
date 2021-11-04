9cbf753c87c576cdea48df14e9b16145f685a1fb
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-04 11:54:52 +0100

    create a method to get a single chat and all the information inside

--- js/script.js
+++ js/script.js
@@ -8,21 +8,68 @@ window.addEventListener("DOMContentLoaded", function(){
             chatList: [
                 {
                     userName: "Marco",
-                    profilePic: "img/avatar_1.jpg"
+                    profilePic: "img/avatar_1.jpg",
+                    messages: [
+                        {
+                            text: "sono Marco",
+                            status: "recieved"
+                        },
+                        {
+                            text: "inviato",
+                            status: "sent"
+                        }
+                    ]
                 },
                 {
                     userName: "Luca",
-                    profilePic: "img/avatar_2.jpg"
+                    profilePic: "img/avatar_2.jpg",
+                    messages: [
+                        {
+                            text: "sono Luca",
+                            status: "recieved"
+                        },
+                        {
+                            text: "inviato",
+                            status: "sent"
+                        }
+                    ]
                 },
                 {
                     userName: "Giovanni",
-                    profilePic: "img/avatar_3.jpg"
+                    profilePic: "img/avatar_3.jpg",
+                    messages: [
+                        {
+                            text: "sono Giovanni",
+                            status: "recieved"
+                        },
+                        {
+                            text: "inviato",
+                            status: "sent"
+                        }
+                    ]
                 },
                 {
                     userName: "Luigi",
-                    profilePic: "img/avatar_4.jpg"
+                    profilePic: "img/avatar_4.jpg",
+                    messages: [
+                        {
+                            text: "sono Luigi",
+                            status: "recieved"
+                        },
+                        {
+                            text: "inviato",
+                            status: "sent"
+                        }
+                    ]
                 }
-            ]
+            ],
+            activeChat: null
+        },
+        methods:{
+            onChatClick(chat){
+                this.activeChat = chat;
+                console.log(`questa è la chat di ${this.activeChat.userName}`, this.activeChat);
+            }
         }
     })
 })
